10e5b32dafc2c09e9037922f58f64a54d159242a
eniola4031 <agboola.eniola4031@gmail.com>
2021-07-21 17:21:15 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SCA Cloud School Application' });
+  res.render('index', { title: 'Welcome to SCA Cloud School Application' });
 });
 
 module.exports = router;
